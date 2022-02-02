21019ee8422f6d60c5cbff1c798ee36d46a23327
ruifernandees <dev.ruifernandes@gmail.com>
2022-02-02 11:45:03 -0300

    feat(server): multithread version
    Co-authored-by: PedroAugustoACT <pedro.augusto.dr11@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,23 +1,36 @@
 import socket
 import json
+from _thread import *
+import threading
 
-address = ("localhost", 3335)
-
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-serverSocket.bind(address)
-serverSocket.listen(1)
-serverInput, address = serverSocket.accept()
-print("Nova conexão: ", address)
-
-while True:
-  response = serverInput.recv(1024)
-  response = json.loads(response.rstrip())
-  if (not ('message' in response)):
-    print("Bem vindo(a), ", response['username'])
-  else:
-    if (response['message'] != 'desconectar'):
-      print("Mensagem do cliente: ", response)
+def threaded(connection):
+  while True:
+    data = connection.recv(1024)
+    response = json.loads(data.rstrip())
+    if (not ('message' in response)):
+      print("Bem vindo(a), ", response['username'])
     else:
-      serverSocket.close()
-      break
+      if (response['message'] != 'desconectar'):
+        print(response['username'], ": ", response['message'])
+      else:
+        print(response['username'], "saiu!")
+        break
+    data = data[::-1]
+    connection.send(data)
+  connection.close()
+
+def main():
+  address = ("localhost", 3335)
+  serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  serverSocket.bind(address)
+  serverSocket.listen(5)
+  while True:
+    print("Aguardando novas conexões")
+    serverInput, address = serverSocket.accept()
+    print("After accept")
+    print("Nova conexão: ", address)
+    start_new_thread(threaded, (serverInput,))
+  serverSocket.close()
+
+if __name__ == '__main__':
+  main()
